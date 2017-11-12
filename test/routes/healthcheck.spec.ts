6fac982011d916b8366a6b6d9827df1d5c408b5e
patricknolan <patrick.nolan@deltareadytechnology.com.au>
2017-11-12 11:59:34 -0500

    Added debug config for tests

--- test/routes/healthcheck.spec.ts
+++ test/routes/healthcheck.spec.ts
@@ -6,15 +6,15 @@ import { server as api } from '../../src/server';
 const expect = chai.expect;
 
 describe('Healthcheck', () => {
-  describe('GET /healthcheck', () => {
-    it('should respond with a HTTP 200 OK', (done) => {
-      request(api)
-        .get('/healthcheck')
-        .expect(200, done)
+    describe('GET /healthcheck', () => {
+        it('should respond with a HTTP 200 OK', (done) => {
+            request(api)
+                .get('/healthcheck')
+                .expect(200, done)
+        });
     });
-  });
 
-  /* If we don't shutdown the server mocha will hang as the restify server is still running */
-  after(() => api.close())
+    /* If we don't shutdown the server mocha will hang as the restify server is still running */
+    after(() => api.close())
 });
 
