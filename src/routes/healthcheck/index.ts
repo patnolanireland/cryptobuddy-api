cbb9ab8181f217bd5f5eb5f53ff8b0211a315ccd
patricknolan <patrick.nolan@deltareadytechnology.com.au>
2018-02-12 21:40:37 -0500

    Added a machine id property to the healthcheck endpoint body to distingues a host in a docker swarm

--- src/routes/healthcheck/index.ts
+++ src/routes/healthcheck/index.ts
@@ -2,7 +2,9 @@ import * as restify from 'restify';
 
 const healthcheck =
     (req: restify.Request, res: restify.Response, next: restify.Next) =>  {
-    res.send(200);
+    res.send(200, {
+        mid: process.env.HOSTNAME || '42',
+    });
     return next();
 };
 
